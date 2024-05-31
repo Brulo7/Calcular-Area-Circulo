a90e3cdd32a70d18ee28a9b3b2a8f1b132a0dd79
Brulo7 <lorenzobruni55@gmail.com>
2024-05-31 19:17:54 -0300

    Calculamos el área de un circulo para practicar algoritmos con pseudocodigo

---
+++ Practica-Algoritmos2.py
@@ -0,0 +1,8 @@
+### Practica de algoritmos. Calcular el area de un circulo:
+import math
+### Paso 1
+radio = int(input("Ingrese el radio del circulo: "))
+### Paso 2
+area_circulo = math.pi * (radio ** 2)
+### paso 3
+print("El area del circulo segun el radio ingresado es: ", area_circulo)
